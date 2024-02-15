d68285ab8626e9666ac8f29a85c6f576171d5a89
razzodev <razzo.des@gmail.com>
2024-02-15 17:13:50 +0700

    add initial routes and links

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
-import './App.css'
+import './App.css';
+import { BrowserRouter, Routes, Route,NavLink } from 'react-router-dom'
 
 function App() {
 
   return (
     <>
-    <p>Hello from Lingdex</p>
+      <BrowserRouter>
+      <nav>
+        <ul><NavLink to='/'>home</NavLink> </ul>
+        <ul><NavLink to='/profile'>profile</NavLink> </ul>
+      </nav>
+        <p>Hello from Lingdex</p>
+        <Routes>
+          <Route path='/profile' element={<h3>profile</h3>}/>
+          <Route path='/' element={<h3>homepage</h3>}/>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
