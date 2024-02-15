50b98e7c1b0160ad634668e78b214a3b8a14789b
razzodev <razzo.des@gmail.com>
2024-02-15 18:16:12 +0700

    apply observer to comp

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,15 @@ import { BrowserRouter, Routes, Route,NavLink } from 'react-router-dom'
 import './App.css';
 import './components/Navbar'
 import Navbar from './components/Navbar';
-
-function App() {
-
+import {observer} from 'mobx-react-lite';
+import { useStore } from './store';
+const App =observer(()=> {
+const {appStore:{contentStore}} = useStore()
   return (
     <>
       <BrowserRouter>
       <Navbar/>
-        <p>Hello from Lingdex</p>
+        <p>current page: {contentStore.currPage}</p>
         <Routes>
           <Route path='/profile' element={<h3>profile</h3>}/>
           <Route path='/' element={<h3>homepage</h3>}/>
@@ -17,6 +18,6 @@ function App() {
       </BrowserRouter>
     </>
   )
-}
+})
 
 export default App
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,13 @@
 import {NavLink} from 'react-router-dom'
 import './Navbar.css';
-
+import { useStore } from '../store';
 function Navbar() {
+  const {appStore:{contentStore}} = useStore()
   return (
     <nav>
         <ul>
-            <li className='navbar-link'><NavLink to='/'>home</NavLink></li>
-            <li className='navbar-link'><NavLink to='/profile'>profile</NavLink></li>
+            <li className='navbar-link' onClick={()=>contentStore.setCurrPage('home!!!')}><NavLink to='/'>home</NavLink></li>
+            <li className='navbar-link'onClick={()=>contentStore.setCurrPage('profile!!!')}><NavLink to='/profile'>profile</NavLink></li>
         </ul>
     </nav>
   )
